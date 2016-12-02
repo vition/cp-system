509a8f648239ab4c2077f69ecda7f7d98497ef0e
vition <369709991@qq.com>
2016-12-02 17:03:10 +0800

    修复推送时用户出错

--- admin/control/projects.php
+++ admin/control/projects.php
@@ -110,7 +110,6 @@
 	$global->query($query);
 	$user->ulog($_SESSION["username"],$control,$_POST);
 	$wxids=$user->getUserId();
-	echo $wxids;
 	$data=array("touser"=>"{$wxids}","msgtype"=> "news","agentid"=> 0,"news"=>array("articles"=>array(array("title"=>"WTC娱乐营销{$control}：{$_POST["title"]}","description"=>"{$_POST["core"]}【请使用电脑登录".$global->getoption("weburl")."进行查看】","picurl"=>$global->getoption("weburl").$picurl))),"safe"=>"0");
 	$weixin->send($data);
 	echo "操作成功了，真棒！";
