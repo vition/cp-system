b957f964f68cc7eac2d7c99e5e84b09e84307209
vition <369709991@qq.com>
2016-12-05 12:52:45 +0800

    临时调试

--- control/getprojects.php
+++ control/getprojects.php
@@ -92,7 +92,7 @@
 			$query="SELECT * FROM `projects` WHERE {$condition} ORDER BY `date` DESC";
 		//}
 		
-		//echo $query;
+		echo $query;
 		$everyPage=12;
 		$countResult=$global->query($query);
 		//echo $query;
