61340b05bd7f35786af6c82b88f455cb772ccc81
vition <369709991@qq.com>
2016-12-07 16:03:09 +0800

    “修改日志管理功能
    git commit -m  “相同项目名称不能重复

--- admin/control/projects.php
+++ admin/control/projects.php
@@ -10,107 +10,114 @@
 		$global->gotopage($global->getoption("weburl"));
 	}
 	//$global
-	switch($_POST["ptype"]){
-		case "insert":
-		$picurl="";
-		$dataKey="`date`,";
-		$dataVal="'".date("Y-m-d H:i:s",time())."',";
-		foreach($_POST as $key=>$val){
-			if($key=="ptype" || $key=="id" || $key=="pdf"){
-				
-			}elseif($key=="content"){
-				$dataKey.="`{$key}`,";
-				$dataVal.="'".$global->en_quotes($val)."',";
-			}else{
-				$dataKey.="`{$key}`,";
-				if($key=="cover"){
-					 if($val!=""){
-						preg_match("/http/",$val,$array);
-						if(empty($array)){
-							 $picurl=$global->blob2Img($val);
-							 $dataVal.="'".$picurl."',";
+	$isresult=$global->query("SELECT `id` FROM `projects` WHERE `title`='{$_POST["title"]}'");
+	if($isresult->num_rows>0 && $_POST["ptype"]=="insert"){
+		echo "{$_POST["title"]} 该IP已存在，不能再新建";
+	}else{
+		switch($_POST["ptype"]){
+			case "insert":
+			$picurl="";
+			$dataKey="`date`,";
+			$dataVal="'".date("Y-m-d H:i:s",time())."',";
+			foreach($_POST as $key=>$val){
+				if($key=="ptype" || $key=="id" || $key=="pdf"){
+					
+				}elseif($key=="content"){
+					$dataKey.="`{$key}`,";
+					$dataVal.="'".$global->en_quotes($val)."',";
+				}else{
+					$dataKey.="`{$key}`,";
+					if($key=="cover"){
+						 if($val!=""){
+							preg_match("/http/",$val,$array);
+							if(empty($array)){
+								 $picurl=$global->blob2Img($val);
+								 $dataVal.="'".$picurl."',";
+							}else{
+								 $dataVal.="'{$val}',";
+								 $picurl=$val;
+							}
+							
+						 }else{
+							 $dataVal.="'',";
+						 }
+					}elseif($key=="pdfname"){
+						if($val!=""){
+							$fileInfo=$global->blob2file($_POST["pdf"],$val,"../../upload/pdf/");
+							if($fileInfo["zhName"]!=""){
+								$dataVal.="'{$fileInfo["zhName"]}',";
+							}
 						}else{
-							 $dataVal.="'{$val}',";
-							 $picurl=$val;
-						}
-						
-					 }else{
-						 $dataVal.="'',";
-					 }
-				}elseif($key=="pdfname"){
-					if($val!=""){
-						$fileInfo=$global->blob2file($_POST["pdf"],$val,"../../upload/pdf/");
-						if($fileInfo["zhName"]!=""){
-							$dataVal.="'{$fileInfo["zhName"]}',";
+							$dataVal.="'',";
 						}
 					}else{
-						$dataVal.="'',";
-					}
-				}else{
-					$dataVal.="'{$val}',";
-					if($key=="tags"){
-						if($val!=""){
-							$global->upTags($val);
+						$dataVal.="'{$val}',";
+						if($key=="tags"){
+							if($val!=""){
+								$global->upTags($val);
+							}
 						}
 					}
+					
 				}
-				
 			}
-		}
-		$query="INSERT INTO `projects`(".rtrim($dataKey,",").",`publisher`) VALUES (".rtrim($dataVal,",").",'{$_SESSION["username"]}')";
-		//echo $query;
-		$control="新增项目";
-		break;
-		case "update":
-		$dataKey="`date`,";
-		$dataVal="'".date("Y-m-d H:i:s",time())."',";
-		
-		$datakv="`date`='".date("Y-m-d H:i:s",time())."',";
-		foreach($_POST as $key=>$val){
-			if($key=="ptype" || $key=="id" || $key=="pdf"){
-				
-			}elseif($key=="content"){
-				$datakv.="`{$key}`='".$global->en_quotes($val)."',";
-			}else{
-				if($key=="cover"){
-					if($val!=""){
-						preg_match("/data/",$val,$array);
-						if(!empty($array)){
-							$picurl=$global->blob2Img($val);
-							$datakv.="`{$key}`='".$picurl."',";
+			$query="INSERT INTO `projects`(".rtrim($dataKey,",").",`publisher`) VALUES (".rtrim($dataVal,",").",'{$_SESSION["username"]}')";
+			//echo $query;
+			$control="新增项目";
+			break;
+			case "update":
+			$dataKey="`date`,";
+			$dataVal="'".date("Y-m-d H:i:s",time())."',";
+			
+			$datakv="`date`='".date("Y-m-d H:i:s",time())."',";
+			foreach($_POST as $key=>$val){
+				if($key=="ptype" || $key=="id" || $key=="pdf"){
+					
+				}elseif($key=="content"){
+					$datakv.="`{$key}`='".$global->en_quotes($val)."',";
+				}else{
+					if($key=="cover"){
+						if($val!=""){
+							preg_match("/data/",$val,$array);
+							if(!empty($array)){
+								$picurl=$global->blob2Img($val);
+								$datakv.="`{$key}`='".$picurl."',";
+							}else{
+								$datakv.="`{$key}`='{$val}',";
+								$picurl=$val;
+							}
 						}else{
-							$datakv.="`{$key}`='{$val}',";
-							$picurl=$val;
+							$picurl="";
+						}
+					}elseif($key=="pdfname"){
+						if($val!="" && isset($_POST["pdf"])){
+							$fileInfo=$global->blob2file($_POST["pdf"],$val,"../../upload/pdf/");
+							if($fileInfo["zhName"]!=""){
+								$datakv.="`{$key}`='{$fileInfo["zhName"]}',";
+							}
+						}
+					}else{
+						$datakv.="`{$key}`='{$val}',";
+						if($key=="tags"){
+							if($val!=""){
+								$global->upTags($val);
+							}
 						}
 					}
-				}elseif($key=="pdfname"){
-					if($val!="" && isset($_POST["pdf"])){
-						$fileInfo=$global->blob2file($_POST["pdf"],$val,"../../upload/pdf/");
-						if($fileInfo["zhName"]!=""){
-							$datakv.="`{$key}`='{$fileInfo["zhName"]}',";
-						}
-					}
-				}else{
-					$datakv.="`{$key}`='{$val}',";
-					if($key=="tags"){
-						if($val!=""){
-							$global->upTags($val);
-						}
-					}
+					
 				}
-				
 			}
+			$query="UPDATE `projects` SET ".rtrim($datakv,",")." WHERE `id`='{$_POST["id"]}'";
+			$control="更新项目";
+			//$query="INSERT INTO `projects`(".rtrim($dataKey,",").") VALUES (".rtrim($dataVal,",").")";
+			break;
 		}
-		$query="UPDATE `projects` SET ".rtrim($datakv,",")." WHERE `id`='{$_POST["id"]}'";
-		$control="更新项目";
-		//$query="INSERT INTO `projects`(".rtrim($dataKey,",").") VALUES (".rtrim($dataVal,",").")";
-		break;
+		$global->query($query);
+		$user->ulog($_SESSION["username"],$control,$_POST);
+		$wxids=$user->getUserId();
+		$data=array("touser"=>"{$wxids}","msgtype"=> "news","agentid"=> 0,"news"=>array("articles"=>array(array("title"=>"WTC娱乐营销{$control}：{$_POST["title"]}","description"=>"{$_POST["core"]}【请使用电脑登录".$global->getoption("weburl")."进行查看】","picurl"=>$global->getoption("weburl").$picurl))),"safe"=>"0");
+		$weixin->send($data);
+		echo "操作成功了，真棒！";
 	}
-	$global->query($query);
-	$user->ulog($_SESSION["username"],$control,$_POST);
-	$wxids=$user->getUserId();
-	$data=array("touser"=>"{$wxids}","msgtype"=> "news","agentid"=> 0,"news"=>array("articles"=>array(array("title"=>"WTC娱乐营销{$control}：{$_POST["title"]}","description"=>"{$_POST["core"]}【请使用电脑登录".$global->getoption("weburl")."进行查看】","picurl"=>$global->getoption("weburl").$picurl))),"safe"=>"0");
-	//$weixin->send($data);
-	echo "操作成功了，真棒！";
-	//echo $query;
+	
 ?>
